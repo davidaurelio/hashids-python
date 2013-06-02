46845fed30add2fa7c47d7b29e3e319276cc85d4
David Aurelio <dev@david-aurelio.com>
2013-06-02 22:52:57 +0200

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 # coding=utf-8
+from __future__ import with_statement
 from distutils.core import setup
+
+
+with open('README.rst') as file:
+    long_description = file.read()
 
 setup(name='hashids',
       version='0.8.0',
       description='Python implementation of hashids (http://www.hashids.org).'
                   'Compatible with python 2.5--3.',
+      long_description=long_description,
       author='David Aurelio',
       author_email='dev@david-aurelio.com',
       url='https://github.com/davidaurelio/hashids-python',
