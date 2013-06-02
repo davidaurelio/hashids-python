d83c2ca544fba984bd924bdf66ed173c869c49ce
David Aurelio <dev@david-aurelio.com>
2013-06-02 14:50:02 +0200

    Complete decryption code

--- hashids.py
+++ hashids.py
@@ -1,122 +1,227 @@
+"""Implements the hashids algorithm in python. For more information, visit
+http://hashids.org/. Compatible with Python 2.5--3"""
 from itertools import chain
+import re
 
-def _is_uint(n):
+# Python 2/3 compatibility code
+try:
+    _STR_TYPE = basestring
+except NameError:
+    _STR_TYPE = str
+
+def _head(iterable):
+    """Extracts the first value from an iterable."""
+    # Python < 2.6 does not have `next()`
+    # Python 3 does not have generator.next()
+    for value in iterable:
+        return value
+
+# end of compatibility code
+
+def _is_str(candidate):
+    """Returns whether a value is a string."""
+    return isinstance(candidate, _STR_TYPE)
+
+def _is_uint(number):
+    """Returns whether a value is an unsigned integer."""
     try:
-        return n == int(n) and n >= 0
+        return number == int(number) and number >= 0
     except ValueError:
         return False
 
-def _replace_index(list, index, object):
-    list.insert(index, object)
-    return list.pop(index + 1)
+def _replace_index(list_object, index, value):
+    """Replaces a value in a list_object with another value. Returns the
+    replaced value."""
+    list_object.insert(index, value)
+    return list_object.pop(index + 1)
 
-def _to_front(element, iterator):
-    return chain((element,), (x for x in iterator if x != element))
+def _to_front(value, iterator):
+    """Yields `value`, then all other elements from `iterator` if they are not
+    equal to `value`."""
+    return chain((value,), (x for x in iterator if x != value))
 
 def _hash(number, alphabet):
-    h = ''
-    n = len(alphabet)
+    """Hashes `number` using the given `alphabet` sequence."""
+    hashed = ''
+    len_alphabet = len(alphabet)
     while True:
-        h = alphabet[number % n] + h
-        number //= n
+        hashed = alphabet[number % len_alphabet] + hashed
+        number //= len_alphabet
         if not number:
-            return h
+            return hashed
 
-def _consistent_shuffle(iterable, salt):
+def _unhash(hashed, alphabet):
+    """Restores a number tuple from hashed using the given `alphabet` index."""
+    number = 0
+    len_hash = len(hashed)
+    len_alphabet = len(alphabet)
+    for i, character in enumerate(hashed):
+        position = alphabet.index(character)
+        number += position * len_alphabet ** (len_hash - i - 1)
+
+    return number
+
+def _reorder(iterable, salt):
+    """Yields all values from `iterable` in an order derived from the
+    given `salt`."""
     sorting = [ord(x) for x in salt] if salt else [0]
-    n = len(sorting)
-    l = list(iterable)
+    len_sorting = len(sorting)
+    values = list(iterable)
 
-    for i in range(n):
+    for i in range(len_sorting):
         add = True
-        for k in range(i, n + i - 1):
-            diff = sorting[(k + 1) % n]
+        for k in range(i, len_sorting + i - 1):
+            diff = sorting[(k + 1) % len_sorting]
             sorting[i] += diff + (k * i) if add else -diff
             add = not add
 
         sorting[i] = abs(sorting[i])
 
     i = -1
-    while l:
-        i = (i + 1) % n
-        pos = sorting[i] % len(l)
-        yield l.pop(pos)
+    while values:
+        i = (i + 1) % len_sorting
+        pos = sorting[i] % len(values)
+        yield values.pop(pos)
+
+def _re_class(characters):
+    """Creates a regular expression with a character class matching
+    all `characters`."""
+    return re.compile('[%s]' % re.escape(''.join(characters)))
 
 class Hashids(object):
+    """Hashes and restores values using the "hashids"
+    algorithm (http://hashids.org/)."""
     PRIMES = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43)
 
     def __init__(self, salt='', min_length=0,
                  alphabet='xcS4F6h89aUbideAI7tkynuopqrXCgTE5GBKHLMjfRsz'):
+        """
+        Initializes a Hashids object with salt, minimum length, and alphabet.
+
+        :param salt: A string influencing the generated hash ids.
+        :param min_length: The minimum length for generated hashes
+        :param alphabet: The characters to use for the generated hash ids.
+        """
         alphabet = [x for i, x in enumerate(alphabet) if alphabet.index(x) == i]
         self._min_length = max(int(min_length), 0)
         self._salt = salt
 
-        self._guards = guards = []
         len_alphabet = len(alphabet)
-        self._separators = separators = [_replace_index(alphabet, prime - 1, None)
-                                         for prime in self.PRIMES
-                                         if prime - 1 < len_alphabet]
-        for index in (0, 4, 8, 12):
-            if index < len(separators):
-                guards.append(separators.pop(index))
+        separators = [_replace_index(alphabet, prime - 1, None)
+                      for prime in self.PRIMES if prime - 1 < len_alphabet]
 
-        self._alphabet = tuple(_consistent_shuffle(filter(bool, alphabet), salt))
+        self._guards = guards = tuple(separators.pop(index)
+                                      for index in (0, 4, 8, 12)
+                                      if index < len(separators))
+        self._guards_re = _re_class(guards)
+        self._separators = tuple(separators)
+        self._separators_re = _re_class(separators)
+        alphabet = (x for x in alphabet if x)
+        self._alphabet = tuple(_reorder(alphabet, salt))
 
     def encrypt(self, *values):
-        if not (values and all(map(_is_uint, values))):
+        """Builds a hash from the passed `values`.
+
+        :param values The values to transform into a hashid
+
+        >>> hashids = Hashids('arbitrary salt', 16, 'abcdefghijkl')
+        >>> hashids.encrypt(1, 23, 456)
+        'fhblhkfjejddjbdl'
+        """
+        if not (values and all(_is_uint(x) for x in values)):
             return ''
 
-        return self._encode(values, self._alphabet, self._salt,
-                            self._min_length)
+        hashed, alphabet = self._encode(values, self._alphabet, self._salt)
+        return self._ensure_length(hashed, values, alphabet)
 
-    def _encode(self, values, alphabet, salt, min_length=0):
-        num_length = len(values)
+    def _encode(self, values, alphabet, salt):
+        """Helper method that does the hash building without argument checks."""
+        len_values = len(values)
         str_values = [str(x) for x in values]
-        separators = list(_consistent_shuffle(self._separators,
-                                              ''.join(str_values)))
-        num_separators = len(separators)
+        separators = list(_reorder(self._separators, ''.join(str_values)))
+        len_separators = len(separators)
 
         lottery_salt = '-'.join(chain(str_values,
                                       (str((v + 1) * 2) for v in values)))
-        lottery = list(_consistent_shuffle(alphabet, lottery_salt))
-        hashed = lottery_char = lottery[0]
+        hashed = lottery_char = _head(_reorder(alphabet, lottery_salt))
         alphabet = list(_to_front(lottery_char, alphabet))
 
         for i, value in enumerate(values):
-            alphabet_salt = str(ord(lottery_char) & 12345) + salt
-            alphabet = list(_consistent_shuffle(alphabet, alphabet_salt))
+            alphabet_salt = '%d%s' % (ord(lottery_char) & 12345, salt)
+            alphabet = list(_reorder(alphabet, alphabet_salt))
             hashed += _hash(value, alphabet)
 
-            if i < num_length - 1:
-                hashed += separators[(value + i) % num_separators]
+            if i < len_values - 1:
+                hashed += separators[(value + i) % len_separators]
 
-        length = len(hashed)
-        if length < min_length:
-            first_index = sum((i + 1) * value
-                              for i, value in enumerate(values))
+        return hashed, alphabet
+
+    def _ensure_length(self, hashid, values, alphabet):
+        """Helper method that extends a hashid if it does not have the
+        minimum lenght."""
+        length = self._min_length
+        salt = self._salt
+        len_hashed = len(hashid)
+        if len_hashed < length:
+            first_index = sum((i + 1) * value for i, value in enumerate(values))
 
             guards = self._guards
-            num_guards = len(guards)
-            guard_index = first_index % num_guards
-            hashed = guards[guard_index] + hashed
-            length += 1
+            len_guards = len(guards)
+            guard_index = first_index % len_guards
+            hashid = guards[guard_index] + hashid
+            len_hashed += 1
 
-            if length < min_length:
-                hashed += guards[(guard_index + length) % num_guards]
-                length += 1
+            if len_hashed < length:
+                hashid += guards[(guard_index + len_hashed) % len_guards]
+                len_hashed += 1
 
-        while length < min_length:
+        while len_hashed < length:
             pad = ord(alphabet[1]), ord(alphabet[0])
-            pad_left = self._encode(pad, alphabet, salt)
-            pad_right = self._encode(pad, alphabet, '%d%d' % pad)
-            hashed = pad_left + hashed + pad_right
+            pad_left = self._encode(pad, alphabet, salt)[0]
+            pad_right = self._encode(pad, alphabet, '%d%d' % pad)[0]
+            hashid = pad_left + hashid + pad_right
 
-            length = len(hashed)
-            excess = length - min_length
+            len_hashed = len(hashid)
+            excess = len_hashed - length
             if excess > 0:
-                hashed = hashed[excess//2:-excess//2]
+                hashid = hashid[excess//2:-excess//2]
 
-            alphabet = list(_consistent_shuffle(alphabet, salt + hashed))
+            alphabet = list(_reorder(alphabet, salt + hashid))
 
-        return hashed
+        return hashid
 
+    def decrypt(self, hashid):
+        """Restore a tuple of numbers from the passed `hashid`.
+
+        :param hashid The hashid to decrypt
+
+        >>> hashids = Hashids('arbitrary salt', 16, 'abcdefghijkl')
+        >>> hashids.decrypt('fhblhkfjejddjbdl')
+        (1, 23, 456)
+        """
+        if not hashid or not _is_str(hashid):
+            return ()
+        try:
+            return tuple(self._decode(hashid))
+        except:
+            return ()
+
+    def _decode(self, hashid):
+        """Helper method that restores the values encoded in a hashid without
+        argument checks."""
+        parts = self._guards_re.split(hashid)
+        hashid = parts[1] if 2 <= len(parts) <= 3 else parts[0]
+
+        lottery_char = None
+        hash_parts = self._separators_re.split(hashid)
+        for part in ((i, x) for i, x in enumerate(hash_parts) if x):
+            i, sub_hash = part
+            if i == 0:
+                lottery_char = hashid[0]
+                sub_hash = sub_hash[1:]
+                alphabet = _to_front(lottery_char, self._alphabet)
+
+            if lottery_char and alphabet:
+                salt = '%d%s' % (ord(lottery_char) & 12345, self._salt)
+                alphabet = list(_reorder(alphabet, salt))
+                yield _unhash(sub_hash, alphabet)
--- test/test_hashids.py
+++ test/test_hashids.py
@@ -1,56 +1,116 @@
 from hashids import Hashids
 
-def test_empty_call():
-    assert Hashids().encrypt() == ''
+class TestEncryption(object):
+    def test_empty_call(self):
+        assert Hashids().encrypt() == ''
 
-def test_default_salt():
-    assert Hashids().encrypt(1, 2, 3) == 'katKSA'
+    def test_default_salt(self):
+        assert Hashids().encrypt(1, 2, 3) == 'katKSA'
 
-def test_single_number():
-    h = Hashids()
-    assert h.encrypt(12345) == 'rGAx'
-    assert h.encrypt(1) == 'yE'
-    assert h.encrypt(22) == 'B8'
-    assert h.encrypt(333) == '7G9'
-    assert h.encrypt(9999) == 'zpz5'
+    def test_single_number(self):
+        h = Hashids()
+        assert h.encrypt(12345) == 'rGAx'
+        assert h.encrypt(1) == 'yE'
+        assert h.encrypt(22) == 'B8'
+        assert h.encrypt(333) == '7G9'
+        assert h.encrypt(9999) == 'zpz5'
 
-def test_multiple_numbers():
-    h = Hashids()
-    assert h.encrypt(683, 94108, 123, 5) == '6nph8p9duq8u9'
-    assert h.encrypt(1, 2, 3) == 'katKSA'
-    assert h.encrypt(2, 4, 6) == '5jhof9'
-    assert h.encrypt(99, 25) == 'nq4CG'
+    def test_multiple_numbers(self):
+        h = Hashids()
+        assert h.encrypt(683, 94108, 123, 5) == '6nph8p9duq8u9'
+        assert h.encrypt(1, 2, 3) == 'katKSA'
+        assert h.encrypt(2, 4, 6) == '5jhof9'
+        assert h.encrypt(99, 25) == 'nq4CG'
 
-def test_salt():
-    h = Hashids(salt='Arbitrary string')
-    assert h.encrypt(683, 94108, 123, 5) == 'q9khp7X9u6BuE'
-    assert h.encrypt(1, 2, 3) == 'a7tLSG'
-    assert h.encrypt(2, 4, 6) == 'Xbh4fp'
-    assert h.encrypt(99, 25) == 'K6nCz'
+    def test_salt(self):
+        h = Hashids(salt='Arbitrary string')
+        assert h.encrypt(683, 94108, 123, 5) == 'q9khp7X9u6BuE'
+        assert h.encrypt(1, 2, 3) == 'a7tLSG'
+        assert h.encrypt(2, 4, 6) == 'Xbh4fp'
+        assert h.encrypt(99, 25) == 'K6nCz'
 
-def test_alphabet():
-    h = Hashids(alphabet='!"#%&\',-/0123456789:;<=>ABCDEFGHIJKLMNOPQRSTUVWXYZ_`abcdefghijklmnopqrstuvwxyz~')
-    assert h.encrypt(2839, 12, 32, 5) == '!%u#Y=%#v'
-    assert h.encrypt(1, 2, 3) == ':Y9c#2'
-    assert h.encrypt(23832) == 'cZKL'
-    assert h.encrypt(99, 25) == 'aNCEI'
+    def test_alphabet(self):
+        h = Hashids(alphabet='!"#%&\',-/0123456789:;<=>ABCDEFGHIJKLMNOPQRSTUVWXYZ_`abcdefghijklmnopqrstuvwxyz~')
+        assert h.encrypt(2839, 12, 32, 5) == '!%u#Y=%#v'
+        assert h.encrypt(1, 2, 3) == ':Y9c#2'
+        assert h.encrypt(23832) == 'cZKL'
+        assert h.encrypt(99, 25) == 'aNCEI'
 
-def test_min_length():
-    h = Hashids(min_length=25)
-    assert h.encrypt(7452, 2967, 21401) == '4ARhAecbrrGh8K7FBBbi4nkhL'
-    assert h.encrypt(1, 2, 3) == 'IeRX9XtbpTkatKSAcXe4tALde'
-    assert h.encrypt(6097) == 'aULxKgxFpEi7prdcK7LFLz4Lk'
-    assert h.encrypt(99, 25) == 'UrBa8pCqLTnq4CGTaMpC7Kj6x'
+    def test_min_length(self):
+        h = Hashids(min_length=25)
+        assert h.encrypt(7452, 2967, 21401) == '4ARhAecbrrGh8K7FBBbi4nkhL'
+        assert h.encrypt(1, 2, 3) == 'IeRX9XtbpTkatKSAcXe4tALde'
+        assert h.encrypt(6097) == 'aULxKgxFpEi7prdcK7LFLz4Lk'
+        assert h.encrypt(99, 25) == 'UrBa8pCqLTnq4CGTaMpC7Kj6x'
 
-def test_all_parameters():
-    h = Hashids('arbitrary salt', 16, 'abcdefghijklmnopqrstuvwxyz')
-    assert h.encrypt(7452, 2967, 21401) == 'mjpnilkonugzfjub'
-    assert h.encrypt(1, 2, 3) == 'nqyjbjpcfeymvfiq'
-    assert h.encrypt(60125) == 'vxwfjmrnfvtmpdow'
-    assert h.encrypt(99, 25) == 'hsnymlyueozbnijs'
+    def test_all_parameters(self):
+        h = Hashids('arbitrary salt', 16, 'abcdefghijklmnopqrstuvwxyz')
+        assert h.encrypt(7452, 2967, 21401) == 'mjpnilkonugzfjub'
+        assert h.encrypt(1, 2, 3) == 'nqyjbjpcfeymvfiq'
+        assert h.encrypt(60125) == 'vxwfjmrnfvtmpdow'
+        assert h.encrypt(99, 25) == 'hsnymlyueozbnijs'
 
-def test_negative_call():
-    assert Hashids().encrypt(1, -2, 3) == ''
+    def test_negative_call(self):
+        assert Hashids().encrypt(1, -2, 3) == ''
 
-def test_float_call():
-    assert Hashids().encrypt(1, 2.5, 3) == ''
+    def test_float_call(self):
+        assert Hashids().encrypt(1, 2.5, 3) == ''
+
+class TestDecryption(object):
+    def test_empty_string(self):
+        assert Hashids().decrypt('') == ()
+
+    def test_non_string(self):
+        assert Hashids().decrypt(object()) == ()
+
+    def test_default_salt(self):
+        assert Hashids().decrypt('katKSA') == (1, 2, 3)
+
+    def test_empty_call(self):
+        assert Hashids().decrypt('') == ()
+
+    def test_single_number(self):
+        h = Hashids()
+        assert h.decrypt('rGAx') == (12345,)
+        assert h.decrypt('yE') == (1,)
+        assert h.decrypt('B8') == (22,)
+        assert h.decrypt('7G9') == (333,)
+        assert h.decrypt('zpz5') == (9999,)
+
+    def test_multiple_numbers(self):
+        h = Hashids()
+        assert h.decrypt('6nph8p9duq8u9') == (683, 94108, 123, 5,)
+        assert h.decrypt('katKSA') == (1, 2, 3,)
+        assert h.decrypt('5jhof9') == (2, 4, 6,)
+        assert h.decrypt('nq4CG') == (99, 25,)
+
+    def test_salt(self):
+        h = Hashids(salt='Arbitrary string')
+        assert h.decrypt('q9khp7X9u6BuE') == (683, 94108, 123, 5,)
+        assert h.decrypt('a7tLSG') == (1, 2, 3,)
+        assert h.decrypt('Xbh4fp') == (2, 4, 6,)
+        assert h.decrypt('K6nCz') == (99, 25,)
+
+    def test_alphabet(self):
+        h = Hashids(alphabet='!"#%&\',-/0123456789:;<=>ABCDEFGHIJKLMNOPQRSTUVWXYZ_`abcdefghijklmnopqrstuvwxyz~')
+        assert h.decrypt('!%u#Y=%#v') == (2839, 12, 32, 5,)
+        assert h.decrypt(':Y9c#2') == (1, 2, 3,)
+        assert h.decrypt('cZKL') == (23832,)
+        assert h.decrypt('aNCEI') == (99, 25,)
+
+    def test_min_length(self):
+        h = Hashids(min_length=25)
+        assert h.decrypt('4ARhAecbrrGh8K7FBBbi4nkhL') == (7452, 2967, 21401,)
+        assert h.decrypt('IeRX9XtbpTkatKSAcXe4tALde') == (1, 2, 3,)
+        assert h.decrypt('aULxKgxFpEi7prdcK7LFLz4Lk') == (6097,)
+        assert h.decrypt('UrBa8pCqLTnq4CGTaMpC7Kj6x') == (99, 25,)
+
+    def test_all_parameters(self):
+        h = Hashids('arbitrary salt', 16, 'abcdefghijklmnopqrstuvwxyz')
+        assert h.decrypt('mjpnilkonugzfjub') == (7452, 2967, 21401,)
+        assert h.decrypt('nqyjbjpcfeymvfiq') == (1, 2, 3,)
+        assert h.decrypt('vxwfjmrnfvtmpdow') == (60125,)
+        assert h.decrypt('hsnymlyueozbnijs') == (99, 25,)
+
+    def test_invalid_hash(self):
+        assert Hashids(alphabet='abcdefghijklm').decrypt('nopqrstuvw') == ()
