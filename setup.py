828f0c5e3cdc5bcca5d1af62e052e2a7e30d1a64
David Aurelio <dev@david-aurelio.com>
2013-06-03 20:18:17 +0200

    Fixing it how it’s supposed to be fixed

--- setup.py
+++ setup.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python
-# coding=utf-8
-from __future__ import with_statement
-from os import path
 from distutils.core import setup
-
-
-with open(path.join(path.dirname(__file__), 'README.txt')) as file:
-    long_description = file.read()
+from os.path import dirname, join
 
 setup(name='hashids',
       version='0.8.3',
       description='Python implementation of hashids (http://www.hashids.org).'
                   'Compatible with python 2.5--3.',
-      long_description=long_description,
+      long_description=open(join(dirname(__file__), 'README.rst')).read(),
       author='David Aurelio',
       author_email='dev@david-aurelio.com',
       url='https://github.com/davidaurelio/hashids-python',
