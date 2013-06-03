cd4bd5b8bd4c1f32fec7f8385727941a83c9549b
David Aurelio <dev@david-aurelio.com>
2013-06-03 09:23:00 +0200

    Update version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(path.dirname(__file__), 'README.txt')) as file:
     long_description = file.read()
 
 setup(name='hashids',
-      version='0.8.1',
+      version='0.8.3',
       description='Python implementation of hashids (http://www.hashids.org).'
                   'Compatible with python 2.5--3.',
       long_description=long_description,
