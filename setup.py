5db8701b0a71f7c19ef2c8183ebd8834ca451efe
David Aurelio <dev@david-aurelio.com>
2014-04-06 23:20:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from os.path import dirname, join
 from codecs import open
 
 setup(name='hashids',
-      version='0.8.4',
+      version='1.0.0',
       description='Python implementation of hashids (http://www.hashids.org).'
-                  'Compatible with python 2.5--3.',
+                  'Compatible with python 2.5-3.',
       long_description=open(join(dirname(__file__), 'README.rst'), encoding='utf-8').read(),
       author='David Aurelio',
       author_email='dev@david-aurelio.com',
