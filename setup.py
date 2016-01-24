98eda2268b24a5ad6fbe91a564ff7d6c8de8c83f
Patrick Mézard <patrick@clustree.com>
2016-01-24 11:52:53 +0100

    setup.py: add python version classifiers
    These classifiers are the only way I have found to programmatically
    know which python version is supported by a given package. This
    information is useful if you write tools to isolate and reduce your
    codebase dependency to python 2.

--- setup.py
+++ setup.py
@@ -12,4 +12,13 @@ setup(name='hashids',
       author_email='dev@david-aurelio.com',
       url='https://github.com/davidaurelio/hashids-python',
       license='MIT License',
-      py_modules=('hashids',))
+      py_modules=('hashids',),
+      classifiers=[
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+      ],)
